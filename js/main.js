cc14a3951e5e25b7b63dfc82d0b2ca16a4836e31
abby-garcia <acg361@gmail.com>
2016-10-03 19:39:10 -0400

    toggle added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,47 @@
+$('#cats').on('click', 'img', function(){
+    $(this).toggleClass("animated flipInX");
+});
 
+$('#hamilton').on('click', function(){
+    $(this).addClass("animated hinge");
+});
+
+$('#lion').on('click', function(){
+    $(this).addClass("animated rollIn");
+});
+
+$('#cabaret').on('click', function(){
+    $(this).addClass("animated zoomInRight");
+});
+
+$('#jesus').on('click', function(){
+    $(this).addClass("animated slideInUp");
+});
+
+$('#wicked').on('click', function(){
+    $(this).addClass("animated zoomInUp");
+});
+
+$('#mormon').on('click', function(){
+    $(this).addClass("animated flipInY");
+});
+
+$('#feet').on('click', function(){
+    $(this).addClass("animated fadeOutRight");
+});
+
+$('#heights').on('click', function(){
+    $(this).addClass("animated lightSpeedIn");
+});
+
+$('#roof').on('click', function(){
+    $(this).addClass("animated rollIn");
+});
+
+$('#purple').on('click', function(){
+    $(this).addClass("animated zoomInRight");
+});
+
+$('#opera').on('click', function(){
+    $(this).addClass("animated slideInDown");
+});
